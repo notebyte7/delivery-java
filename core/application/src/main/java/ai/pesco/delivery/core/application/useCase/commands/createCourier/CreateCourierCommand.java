049aec46da4b6325fc930fbdaf480c4155478998
ieliseev <notebyte@yandex.ru>
2025-06-01 13:50:16 +0300

    module-8

---
+++ core/application/src/main/java/ai/pesco/delivery/core/application/useCase/commands/createCourier/CreateCourierCommand.java
@@ -0,0 +1,4 @@
+package ai.pesco.delivery.core.application.useCase.commands.createCourier;
+
+public record CreateCourierCommand(String name, int speed) {
+}
---
+++ core/application/src/main/java/ai/pesco/delivery/core/application/useCase/commands/createCourier/CreateCourierHandler.java
@@ -0,0 +1,5 @@
+package ai.pesco.delivery.core.application.useCase.commands.createCourier;
+
+public interface CreateCourierHandler {
+    void handle(CreateCourierCommand command);
+}
